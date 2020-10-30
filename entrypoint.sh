690222ec83278640ef931a3527e6ed64729e0bb6
Michael Friedrich <mifriedr@adobe.com>
2020-10-30 12:37:03 -0700

    Fix: Escape glob
    Fixes issues with dirs with large amount of files

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,5 @@ OUTPUT_DIR_STATS=$WORKDIR/jpeg
 mkdir -p "$OUTPUT_DIR_STATS"
 cd "$OUTPUT_DIR_STATS"
 echo "Running ps to jpeg on $WORKDIR/*ps into $OUTPUT_DIR_STATS"
-find "$WORKDIR/" -name *.ps -type f -print0 | xargs -0 -P $NUM_PROC -I {} /bin/bash -c 'if [ -f "$(basename {}).ps" ]; then printf "x"; else gs -sDEVICE=jpeg -sOutputFile="$(basename {}).jpg" -dBATCH -dNOPAUSE -r300 "{}";printf "o"; fi'
+find "$WORKDIR/" -name \*.ps -type f -print0 | xargs -0 -P $NUM_PROC -I {} /bin/bash -c 'if [ -f "$(basename {}).ps" ]; then printf "x"; else gs -sDEVICE=jpeg -sOutputFile="$(basename {}).jpg" -dBATCH -dNOPAUSE -r300 "{}";printf "o"; fi'
 echo "..done"
